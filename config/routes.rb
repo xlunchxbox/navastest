942d75ca18b089ce4b32bbc4ecb550a018ab0b92
Christopher Cruz <chris1cruz@gmail.com>
2013-08-22 15:03:54 -0500

    static views css still working

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Navas::Application.routes.draw do
 
   root "static_pages#home"
   
+  #get "static_pages/home"
+  get "static_pages/pinatas"
+  get "static_pages/snowcones"
+  get "static_pages/popcorn"
+  get "static_pages/cotton_candy"
+  get "static_pages/tables_chairs"
+  
   resources :moonwalks
   resources :contact_forms
   
